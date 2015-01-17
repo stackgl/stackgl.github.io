b7dd0b031c0d130dcd5dfd2f74725ea3ac0e6572
Hugh Kennedy <hughskennedy@gmail.com>
2015-01-17 17:34:31 +0800

    remove splash-flock, update package.json

--- splash-flock/demo.js
+++
@@ -1,7 +0,0 @@
-document.body.style.margin = 0
-document.body.style.padding = 0
-document.body.style.overflow = 'hidden'
-
-require('./')(document.body.appendChild(
-  document.createElement('canvas')
-))
