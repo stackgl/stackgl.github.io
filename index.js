dd852e98bedc8a93d0b579d3b53e1222b74b9d12
Hugh Kennedy <hughskennedy@gmail.com>
2015-01-17 16:41:34 +0800

    move GA into index

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-54792490-1', 'auto');
-ga('send', 'pageview');
-
 var filter    = require('./lib/filter')
 var minstache = require('minstache')
 var domify    = require('domify')
